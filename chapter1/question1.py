5101c5936f2063e53c2d1fa733380b9e9f3738f2
esparig <esparig@gmail.com>
2019-09-13 13:04:27 +0200

    question 1.1 fix time cost on sorting

--- chapter1/question1.py
+++ chapter1/question1.py
@@ -16,11 +16,10 @@ def all_unique_chars_using_set(string: str) -> bool:
 def all_unique_chars_using_bit_vector(string: str) -> bool:
     """Determine if a string has all unique characters using a bit vector.
     - Computational cost: O(n)
-    - Spatial cost: O(n)
+    - Spatial cost: O(1)
     """
     visited_char = 0
     for char in string:
-        print(ord(char), visited_char, visited_char >> ord(char), 1 << ord(char))
         if visited_char >> ord(char) & 1:
             return False
         visited_char = 1 << ord(char) | visited_char
@@ -38,7 +37,7 @@ def all_unique_chars(string: str) -> bool:
     return True
 
 """Other ideas:
-- sort the string and look for repeated chars together: O(n)/O(1), drawbacks: we alter the original string.
+- sort the string and look for repeated chars together: O(n log n)/O(1) if we alter the original string, if not O(n).
 """
         
 import unittest
